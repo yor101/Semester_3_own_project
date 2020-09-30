fa67686120a1d57f96aebd4066dc4e2fba81f1e0
yorick <yorick_laros@outlook.com>
2020-09-30 13:12:12 +0200

    First commit

--- src/Yorick/Laros/Main.java
+++ src/Yorick/Laros/Main.java
@@ -1,9 +1,20 @@
 package Yorick.Laros;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+    Message message = new Message();
+
+        while (message.communicat()){
+            Scanner scanner = new Scanner(System.in);
+            String a = scanner.next();
+            System.out.println("scanner says: " + a);
+
+        }
+
 
 
 
--- src/Yorick/Laros/Message.java
+++ src/Yorick/Laros/Message.java
@@ -1,8 +1,28 @@
 package Yorick.Laros;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Message {
 
-    
+    private Socket socket;
 
+    public boolean communicat() {
+
+        try {
+            socket = new Socket("localhost", 5000);
+
+
+        } catch (
+                IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
+        }
+
+        return socket.isBound();
+    }
 
 }
